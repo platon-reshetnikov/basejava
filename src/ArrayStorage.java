9719aafe1f2f204b90d14f6da8d4414598661a0c
Platon <ctrather2@gmail.com>
2021-08-31 17:26:57 +0300

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,58 @@
+
+import java.util.Arrays;
+
+
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    Integer storageSize = 0;
 
     void clear() {
+        storage = new Resume[10000];
+        storageSize = 0;
     }
 
     void save(Resume r) {
+        storage[storageSize] = r;
+        storageSize++;
     }
 
     Resume get(String uuid) {
+        if(this.storage.equals(uuid)) {
+            return storage[indexOf(uuid)];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        if(storage.equals(uuid)) {
+            Resume[] storageDst = new Resume[storage.length - 1];
+            int remainingElements = storage.length - (indexOf(uuid) + 1);
+            System.arraycopy(storage, 0, storageDst, 0, indexOf(uuid));
+            System.arraycopy(storage, indexOf(uuid) + 1, storageDst, indexOf(uuid), remainingElements);
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(this.storage,0,this.storageSize);
     }
 
     int size() {
-        return 0;
+        return storageSize ;
+    }
+
+    int indexOf(String uuid){
+        for (int i = 0; i < this.storage.length; i++) {
+            if (this.storage.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
